39228bf7eeb285197e763b2dac5ce5f64e09e2aa
Leo Nesfield <leo@thelmgn.com>
2019-07-22 21:51:52 +0100

    finish

--- dogs/Program.cs
+++ dogs/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace dogs {
 
@@ -13,6 +14,15 @@ namespace dogs {
     }
 
     class Program {
+        private static void printCard(Dog card) {
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine($"   Name         : {card.Name}");
+            Console.WriteLine($"1. Exercise     : {card.Exercise}/5");
+            Console.WriteLine($"2. Intelligence : {card.Intelligence}/100");
+            Console.WriteLine($"3. Friendlyness : {card.Friendlyness}/10");
+            Console.WriteLine($"4. Drool        : {card.Drool}/10 (lowest better)");
+            Console.WriteLine("------------------------------------------------");
+        }
         static void Main(string[] args) {
             while (true) {
                 Console.WriteLine("-- Dogs Top Trumps --");
@@ -101,6 +111,7 @@ namespace dogs {
             game(computerDogs,humanDogs);            
         }
         private static void game(List<Dog> computerDogs, List<Dog> humanDogs) {
+            bool humanTurn = true;
             while (true) {
                 if (computerDogs.Count <= 0) {
                     Console.WriteLine("Congratulations! You win!");
@@ -112,7 +123,96 @@ namespace dogs {
                     System.Threading.Thread.Sleep(2000);
                     break;
                 }
+                Console.WriteLine("\n\n\n\n");
                 Console.WriteLine($"CPU: {computerDogs.Count} | YOU: {humanDogs.Count}");
+                Dog computerCard = computerDogs[0];
+                computerDogs.RemoveAt(0);
+                Dog humanCard = humanDogs[0];
+                humanDogs.RemoveAt(0);
+
+                int humanScore;
+                int cpuScore;
+                if (humanTurn) {
+                    Console.WriteLine("It's your turn!");
+                    Console.WriteLine("Your current card:");
+                    printCard(humanCard);
+                    while (true) {
+                        Console.WriteLine("What stat would you like to play? [1-4]");
+                        ConsoleKeyInfo input = Console.ReadKey();
+                        if (input.Key == ConsoleKey.D1) { // Exercise
+                            humanScore = humanCard.Exercise;
+                            cpuScore = computerCard.Exercise;
+                            break;
+                        } else if (input.Key == ConsoleKey.D2) { // Intelligence
+                            humanScore = humanCard.Intelligence;
+                            cpuScore = computerCard.Intelligence;
+                            break;
+                        } else if (input.Key == ConsoleKey.D3) { // Friendlyness
+                            humanScore = humanCard.Friendlyness;
+                            cpuScore = computerCard.Friendlyness;
+                            break;
+                        } else if (input.Key == ConsoleKey.D4) { // Drool
+                            humanScore = 10 - humanCard.Drool;
+                            cpuScore = 10 - computerCard.Drool;
+                            break;
+                        } else {
+                            Console.WriteLine("Invalid key choice!");
+                        }
+                    }
+                } else {
+                    Console.WriteLine("It's the computers turn.");
+                    Console.WriteLine("Your current card:");
+                    printCard(humanCard);
+                    Console.WriteLine("Computers current card:");
+                    printCard(computerCard);
+                    Dictionary<string,float> results = new Dictionary<string,float>();
+                    results.Add("exercise",computerCard.Exercise / 5);
+                    results.Add("intelligence",computerCard.Intelligence / 100);
+                    results.Add("friendlyness",computerCard.Friendlyness / 10);
+                    results.Add("drool",1 - (computerCard.Drool / 10));
+
+                    // pick highest of above
+                    double max = results.Max(kvp => kvp.Value);
+                    string chosen = results.Where(kvp => kvp.Value == max).Select(kvp => kvp.Key).First();
+                    Console.WriteLine($"Computer chose {chosen}");
+                    if (chosen == "exercise") { // Exercise
+                        humanScore = humanCard.Exercise;
+                        cpuScore = computerCard.Exercise;
+                    } else if (chosen == "intelligence") { // Intelligence
+                        humanScore = humanCard.Intelligence;
+                        cpuScore = computerCard.Intelligence;
+                    } else if (chosen == "friendlyness") { // Friendlyness
+                        humanScore = humanCard.Friendlyness;
+                        cpuScore = computerCard.Friendlyness;
+                    } else { // Drool
+                        humanScore = 10 - humanCard.Drool;
+                        cpuScore = 10 - computerCard.Drool;
+                    }
+                }
+                Console.WriteLine("\n\n\n\n");
+                Console.WriteLine("Your card was: ");
+                printCard(humanCard);
+                Console.WriteLine("The computers card was: ");
+                printCard(computerCard);
+                System.Threading.Thread.Sleep(2500);
+                Console.WriteLine("\n\n\n\n");
+                if (humanScore >= cpuScore) {
+                    
+                        Console.WriteLine("You won that round!");
+                        Console.WriteLine("You obtained the card:");
+                        printCard(computerCard);
+                        humanDogs.Add(humanCard);
+                        humanDogs.Add(computerCard);
+                        humanTurn = true;
+                } else {
+                    Console.WriteLine("You lost that round.");
+                        Console.WriteLine("You lost the card:");
+                        printCard(humanCard);
+                        computerDogs.Add(computerCard);
+                        computerDogs.Add(humanCard);
+                        humanTurn = false;
+                }
+                System.Threading.Thread.Sleep(2500);
             }
         }
     }
